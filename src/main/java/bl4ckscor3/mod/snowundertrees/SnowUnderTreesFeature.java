6f6aefddc6c27ad73412551bfd5b048a3e112dfa
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-07-24 11:40:26 +0200

    update class names

--- src/main/java/bl4ckscor3/mod/snowundertrees/SnowUnderTreesFeature.java
+++ src/main/java/bl4ckscor3/mod/snowundertrees/SnowUnderTreesFeature.java
@@ -4,29 +4,29 @@ import java.util.Random;
 
 import com.mojang.serialization.Codec;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LeavesBlock;
-import net.minecraft.block.SnowyDirtBlock;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ISeedReader;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.Heightmap;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.SnowyDirtBlock;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
-public class SnowUnderTreesFeature extends Feature<NoFeatureConfig>
+public class SnowUnderTreesFeature extends Feature<NoneFeatureConfiguration>
 {
-	public SnowUnderTreesFeature(Codec<NoFeatureConfig> codec)
+	public SnowUnderTreesFeature(Codec<NoneFeatureConfiguration> codec)
 	{
 		super(codec);
 	}
 
 	@Override
-	public boolean place(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config)
+	public boolean place(WorldGenLevel world, ChunkGenerator generator, Random rand, BlockPos pos, NoneFeatureConfiguration config)
 	{
-		BlockPos.Mutable mPos = new BlockPos.Mutable();
+		BlockPos.MutableBlockPos mPos = new BlockPos.MutableBlockPos();
 
 		for(int xi = 0; xi < 16; xi++)
 		{
@@ -35,11 +35,11 @@ public class SnowUnderTreesFeature extends Feature<NoFeatureConfig>
 				int x = pos.getX() + xi;
 				int z = pos.getZ() + zi;
 
-				mPos.set(x, world.getHeight(Heightmap.Type.MOTION_BLOCKING, x, z) - 1, z);
+				mPos.set(x, world.getHeight(Heightmap.Types.MOTION_BLOCKING, x, z) - 1, z);
 
 				if(world.getBlockState(mPos).getBlock() instanceof LeavesBlock)
 				{
-					mPos.set(x, world.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z), z);
+					mPos.set(x, world.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, x, z), z);
 
 					if(world.getBiome(mPos).shouldSnow(world, mPos))
 					{
--- src/main/java/bl4ckscor3/mod/snowundertrees/WorldTickHandler.java
+++ src/main/java/bl4ckscor3/mod/snowundertrees/WorldTickHandler.java
@@ -2,19 +2,19 @@ package bl4ckscor3.mod.snowundertrees;
 
 import java.util.Optional;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LeavesBlock;
-import net.minecraft.block.SnowyDirtBlock;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.Heightmap;
-import net.minecraft.world.server.ChunkHolder;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.SnowyDirtBlock;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.server.level.ChunkHolder;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.WorldTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -32,14 +32,14 @@ public class WorldTickHandler
 		{
 			if(event.phase == Phase.START && event.world.isRaining() && Configuration.CONFIG.enableWhenSnowing.get())
 			{
-				ServerWorld world = (ServerWorld)event.world;
+				ServerLevel world = (ServerLevel)event.world;
 
 				world.getChunkSource().chunkMap.getChunks().forEach(chunkHolder -> {
-					Optional<Chunk> optional = chunkHolder.getEntityTickingChunkFuture().getNow(ChunkHolder.UNLOADED_LEVEL_CHUNK).left();
+					Optional<LevelChunk> optional = chunkHolder.getEntityTickingChunkFuture().getNow(ChunkHolder.UNLOADED_LEVEL_CHUNK).left();
 
 					if(optional.isPresent() && world.random.nextInt(16) == 0)
 					{
-						Chunk chunk = optional.get();
+						LevelChunk chunk = optional.get();
 						ChunkPos chunkPos = chunk.getPos();
 						int chunkX = chunkPos.getMinBlockX();
 						int chunkY = chunkPos.getMinBlockZ();
@@ -47,9 +47,9 @@ public class WorldTickHandler
 						Biome biome = world.getBiome(randomPos);
 						boolean biomeDisabled = Configuration.CONFIG.filteredBiomes.get().contains(world.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY).getKey(biome).toString());
 
-						if(!biomeDisabled && world.getBlockState(world.getHeightmapPos(Heightmap.Type.MOTION_BLOCKING, randomPos).below()).getBlock() instanceof LeavesBlock)
+						if(!biomeDisabled && world.getBlockState(world.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, randomPos).below()).getBlock() instanceof LeavesBlock)
 						{
-							BlockPos pos = world.getHeightmapPos(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, randomPos);
+							BlockPos pos = world.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, randomPos);
 							BlockState state = world.getBlockState(pos);
 
 							if(biome.shouldSnow(world, pos) && state.isAir(world, pos))
