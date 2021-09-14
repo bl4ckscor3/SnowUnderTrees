3d7d27e61b0d13d10cc6a9774f8431cf8c428388
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-09-14 12:46:33 +0200

    fix serene seasons and snow real magic handling incompatibility

--- src/main/java/bl4ckscor3/mod/snowundertrees/SnowRealMagicHandler.java
+++ src/main/java/bl4ckscor3/mod/snowundertrees/SnowRealMagicHandler.java
@@ -2,14 +2,13 @@ package bl4ckscor3.mod.snowundertrees;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.LightType;
 import snownee.snow.block.ModSnowBlock;
 
 public class SnowRealMagicHandler
 {
 	public static boolean placeSnow(IWorld world, BlockPos pos)
 	{
-		if(world.getBiome(pos).getTemperature() <= 0.15 && pos.getY() >= 0 && pos.getY() < 256 && world.getLightFor(LightType.BLOCK, pos) < 10)
+		if(world.getBiome(pos).doesSnowGenerate(world, pos))
 			return ModSnowBlock.convert(world, pos, world.getBlockState(pos), 1, 2);
 		else
 			return false;
