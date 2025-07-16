7caa3549815ca2ef59a2963bd87de250574fa8c7
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-16 20:49:32 +0200

    Readd Moonrise compatibility

--- src/main/java/bl4ckscor3/mod/snowundertrees/MoonriseCompat.java
+++ src/main/java/bl4ckscor3/mod/snowundertrees/MoonriseCompat.java
@@ -2,11 +2,12 @@ package bl4ckscor3.mod.snowundertrees;
 
 import java.util.function.Consumer;
 
+import ca.spottedleaf.moonrise.patches.chunk_system.level.ChunkSystemServerLevel;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.LevelChunk;
 
 public class MoonriseCompat {
 	public static void chunkRunner(ServerLevel level, Consumer<LevelChunk> action) {
-		//((ChunkSystemServerLevel) level).moonrise$getTickingChunks().forEach(cah -> action.accept(cah.chunk()));
+		((ChunkSystemServerLevel) level).moonrise$getTickingChunks().forEach(action);
 	}
 }
